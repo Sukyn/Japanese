2703a351fbfbb77f0b155c63cbdd7a6401227469
Sukyn <colin.blake@u-psud.fr>
2020-06-17 19:24:52 +0200

    + added flashcard mechanics + random card picking + giving and asking informations + validating answer

--- flashcard.py
+++ flashcard.py
@@ -1,6 +1,9 @@
 """FLASHCARD PROGRAM"""
 import json
-#import random
+import os
+from random import randint
+
+MAX_LEVEL = 10
 
 class Card:
     """represents a card"""
@@ -11,12 +14,13 @@ class Card:
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+
 class Flashcard():
     """represents a flashcard"""
     #CARDS is used at initialization to collect data from json
     CARDS = []
 
-    def __init__(self, activation=True, progress=0, **kwargs):
+    def __init__(self, activation=True, progress=1, **kwargs):
         #If not activated it shouldn't be pickable
         self._activated = activation
         #The higher skill you have, the lower chance of picking this card you get
@@ -29,11 +33,14 @@ class Flashcard():
 
     def correct(self):
         """called when someone gives a right answer"""
-        self._skill += 1
+        if self._skill < MAX_LEVEL-1:
+            self._skill += 1
+            print_card()
 
     def wrong(self):
         """called when someone gives a wront answer"""
-        self._skill = 0
+        self._skill = 1
+        print_card()
 
     def change_activation(self):
         """called when someone want to activate/deactivate a card"""
@@ -42,23 +49,75 @@ class Flashcard():
         else:
             self._activated = True
 
+    def send_random_information(self):
+        """sends a random information about a card"""
+        str(self)
+        datas = []
+        for key, value in self.card.items():
+            temp = [key,value]
+            datas.append(temp)
+        amount_of_data = len(datas)
+        while True:
+            selected_data = randint(0, amount_of_data-1)
+            if datas[selected_data][0] != "identificator":
+                break
+        return datas[selected_data]
+
+    def send_random_half_information(self):
+        """sends a random information about a card buy without the answer"""
+        data = self.send_random_information()
+        return data[0]
+
+    def validate_answer(self, question, answer):
+        """Tells if an asnwer is the good one"""
+        str(self)
+        if self.card[question] == answer:
+            self.correct()
+            return True
+        else:
+            self.wrong()
+            return False
+
+
+
     @classmethod
     def initialize(cls):
         """Initializes datas"""
         #We read all datas...
-        with open("data/flashcard.json", "r", encoding='utf-8') as read_file:
-            every = json.load(read_file)
-        cls.CARDS = []
-        #... and then we transform it into Flashcards
-        for cardv in every:
-            Flashcard(activation=cardv["_activated"], progress=cardv["_skill"], **cardv["card"])
+        filesize = os.path.getsize("data/flashcard.json")
 
+        if filesize == 0:
+            print("The file is empty, initialization aborted")
+        else:
+            with open("data/flashcard.json", "r", encoding='utf-8') as read_file:
+                every = json.load(read_file)
+            cls.CARDS = []
+            #... and then we transform it into Flashcards
+            for cardv in every:
+                Flashcard(activation=cardv["_activated"], progress=cardv["_skill"], **cardv["card"])
+
+
+    @classmethod
+    def random_card(cls):
+        """Chose a random existing card"""
+        if not cls.CARDS:
+            cls.initialize()
+        # --- The lower skill you have on a card, the more it will be chosen --- #
+        cards = []
+        for card in cls.CARDS:
+            for i in range(MAX_LEVEL-card.skill):
+                cards.append(card)
+        # ---------------------------------------------------------------------- #
+        number_of_cards = len(cards)
+        random_card = randint(0, number_of_cards-1)
+        return cards[random_card]
 
     @classmethod
     def last_identificator(cls):
         """shows last registered identificator"""
         maximum = 0
         for card in cls.CARDS:
+            str(card)
             if card.card["identificator"] > maximum:
                 maximum = card.card["identificator"]
         return maximum
@@ -118,7 +177,17 @@ def delete_card(identificator):
           "of card with identificator : {}".format(identificator))
 
 if __name__ == '__main__':
-    #Flashcard.initialize()
-    add_card(nom="DENIS", prenom="Martin")
-    add_card(profession="pingu")
-    delete_card(1)
+    Flashcard.initialize()
+    selected_card = Flashcard.random_card()
+    given_information = selected_card.send_random_information() #User needs to know a part of a card to guess the other one
+    print("Given information : ", given_information)
+    while True:
+        question = selected_card.send_random_half_information()
+        if question != given_information[0]:
+            break
+    print(question, " ?")
+    answer = input()
+    if selected_card.validate_answer(question, answer):
+        print("GG boy")
+    else:
+        print(":/")
